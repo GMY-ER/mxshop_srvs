61c412eeaf8aeb459c348f35358ffe6e399a7669
古明扬 <gmy@jwzg.com>
2022-07-01 15:37:15 +0800

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module mxshop_srvs
 
 go 1.15
+
+require (
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/gorm v1.23.6
+)
---
+++ user_srv/model/main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"mxshop_srvs/user_srv/model"
+	"os"
+	"time"
+)
+
+func main() {
+	dsn := "root:root@tcp(152.136.121.186:3306)/mxshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second, // 慢 SQL 阈值
+			LogLevel:                  logger.Info, // 日志级别
+			IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  true,        // 禁用彩色打印
+		},
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&model.User{})
+}
